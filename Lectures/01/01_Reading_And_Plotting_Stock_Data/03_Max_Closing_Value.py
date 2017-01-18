4e8b1dddca68c02c35f4d0a4356083263105c3ac
jean28 <jean.mendez2@upr.edu>
2017-01-17 23:25:38 -0500

    Max Closing Val and Mean Volume.

--- Lectures/01/01_Reading_And_Plotting_Stock_Data/03_Max_Closing_Value.py
+++ Lectures/01/01_Reading_And_Plotting_Stock_Data/03_Max_Closing_Value.py
@@ -1,39 +1,20 @@
-''' Read Data Off CSV Files'''
+""" Compute Maximum Closing Price """
 
 import pandas as pd
-import matplotlib.pyplot as plt
 
 def get_max_close(symbol):
-    df = pd.read_csv("data/{}.csv".format(symbol))
-    return df['Close'].max()
-
-
-def get_mean_volume(symbol):
-    """Return the mean volume for stock indicated by symbol.
+    """
+    Return the mean volume for stock indicated by symbol.
 
     Note: Data for a stock is stored in file: data/<symbol>.csv
     """
-    df = pd.read_csv("data/{}.csv".format(symbol))  # read in data
-    return df['Volume'].mean()
+    df = pd.read_csv("../../../data/{}.csv".format(symbol))
+    return df['Close'].max() # compute and return max
 
 def test_run():
-    df = pd.read_csv("../../data/AAPL.csv")
-
-    #plt.show()
-    #print df #print entire dataframe
-    #print df.head() #print
-    #print df.tail()
-    #print df[10:21] #rows between index 10 and 20
-
-    #for symbol in ['AAPL', 'IBM']:
-    #    print "Max Close"
-    #    print symbol, get_max_close(symbol)
-    #    print "Mean Volume"
-    #    print symbol, get_mean_volume(symbol)
-
-    print df['Adj Close']
-    df['Adj Close'].plot()
-    plt.show()
+    for symbol in ['AAPL', 'IBM']:
+        print "Max Close"
+        print symbol, get_max_close(symbol)
 
 if __name__ == "__main__":
     test_run()
