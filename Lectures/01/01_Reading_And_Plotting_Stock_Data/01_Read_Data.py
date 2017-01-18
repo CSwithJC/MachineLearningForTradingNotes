35f3241c91edc6e87899f1c3fdb1ca004d3a2fa8
jean28 <jean.mendez2@upr.edu>
2017-01-17 23:15:37 -0500

    More project organization.

---
+++ Lectures/01/01_Reading_And_Plotting_Stock_Data/01_Read_Data.py
@@ -0,0 +1,12 @@
+""" Import CSV File """
+
+import pandas as pd
+
+def test_run():
+    df = pd.read_csv("../../../data/AAPL.csv")
+    print df # Print Entire DataFrame
+    print df.head() # Print First 5 Rows
+    print df.tail() # Print Last 5 Rows
+
+if __name__ == "__main__":
+    test_run()
