44b233f9bb5df6b4045fbe9541d0eacce261541c
jean28 <jean.mendez2@upr.edu>
2017-01-18 21:31:12 -0500

    Added first code samples for lesson 3.

---
+++ Lectures/01/03_The_Power_of_Numpy/01_Creating_NumPy_Arrays.py
@@ -0,0 +1,13 @@
+""" Creating NumPy Arrays. """
+
+import numpy as np
+
+def test_run():
+    # List to 1D Array
+    print np.array([2, 3, 4])
+
+    # Lists 2D Array
+    print np.array([(2, 3, 4), (5, 6, 7)])
+
+if __name__ == "__main__":
+    test_run()
