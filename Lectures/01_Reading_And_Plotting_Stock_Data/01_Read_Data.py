65d9282ce9684533a4cee3cbb6f9768e18d1d491
jean28 <jean.mendez2@upr.edu>
2017-04-04 23:31:53 -0400

    Deleted useless folders and started fixing the location of the data.

--- Lectures/01_Reading_And_Plotting_Stock_Data/01_Read_Data.py
+++ Lectures/01_Reading_And_Plotting_Stock_Data/01_Read_Data.py
@@ -3,7 +3,7 @@
 import pandas as pd
 
 def test_run():
-    df = pd.read_csv("../../../data/AAPL.csv")
+    df = pd.read_csv("../../data/AAPL.csv")
     print df # Print Entire DataFrame
     print df.head() # Print First 5 Rows
     print df.tail() # Print Last 5 Rows
